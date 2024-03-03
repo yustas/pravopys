dd28912982a26dc47b44e8039e95ff0a5652f04a
yustas <yustas11051977@gmail.com>
2024-03-03 23:44:25 +0200

    Refactored DB module

--- lib/utils/db.dart
+++ lib/utils/db.dart
@@ -7,42 +7,46 @@ import 'package:sqflite/sqlite_api.dart';
 
 const dbName = 'mova.db';
 
-Future<Database> openDatabase() async {
+Future<String> getDatabasePath() async {
   final databasesPath = await syspath.getLibraryDirectory();
-  final dbPath = path.join(databasesPath.path, dbName);
+  return path.join(databasesPath.path, dbName);
+}
 
+Future<void> copyDatabase(String from, String to) async {
+  // Should happen only the first time you launch your application
+  print("Creating new copy from asset");
+
+  // Make sure the parent directory exists
+  try {
+    await Directory(path.dirname(to)).create(recursive: true);
+  } catch (_) {}
+
+  // Copy from asset
+  ByteData data = await rootBundle.load(from);
+  List<int> bytes =
+      data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+  // Write and flush the bytes written
+  await File(to).writeAsBytes(bytes, flush: true);
+}
+
+Future<Database> openDatabase() async {
+  final dbPath = await getDatabasePath();
   var isExists = await sql.databaseExists(dbPath);
 
-  if (true) {
-    // Should happen only the first time you launch your application
-    print("Creating new copy from asset");
-
-    // Make sure the parent directory exists
-    try {
-      await Directory(path.dirname(dbPath)).create(recursive: true);
-    } catch (_) {}
-
-
-    // Copy from asset
-    ByteData data = await rootBundle.load(path.join("assets", "db", dbName));
-    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-
-    // Write and flush the bytes written
-    await File(dbPath).writeAsBytes(bytes, flush: true);
-
-  } else {
-    print("Opening existing database");
-  }
+  true
+      ? await copyDatabase(path.join("assets", "db", dbName), dbPath)
+      : print("Opening existing database");
 
   return await sql.openDatabase(
-      path.join(databasesPath.path, dbName),
-      // version: 1,
-      readOnly: true,
-      // onCreate: (Database db, int version) {
-      //   print("DB was created (");
-      // }
-      // onCreate: (Database db, int version) async {
-      //   await db.execute('CREATE TABLE cnt(value INTEGER)');
+    dbPath,
+    // version: 1,
+    readOnly: true,
+    // onCreate: (Database db, int version) {
+    //   print("DB was created (");
+    // }
+    // onCreate: (Database db, int version) async {
+    //   await db.execute('CREATE TABLE cnt(value INTEGER)');
   );
 }
 
