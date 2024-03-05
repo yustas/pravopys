eb2ee522ad82bb273d75d85863acb9481a053fdf
yustas <yustas11051977@gmail.com>
2024-03-05 21:08:45 +0200

    Logger added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:mova/utils/db.dart';
+import 'package:mova/widgets/content_list.dart';
+import 'package:logging/logging.dart';
 
-void main() => runApp(const FutureBuilderExampleApp());
+void main() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time} | ${record.message}');
+  });
+
+  runApp(const FutureBuilderExampleApp());
+}
 
 class FutureBuilderExampleApp extends StatelessWidget {
   const FutureBuilderExampleApp({super.key});
@@ -18,72 +26,7 @@ class FutureBuilderExampleApp extends StatelessWidget {
         /* dark theme settings */
       ),
       themeMode: ThemeMode.system,
-      home: const Scaffold(
-        body: FutureBuilderExample(),
-      ),
-    );
-  }
-}
-
-class FutureBuilderExample extends StatefulWidget {
-  const FutureBuilderExample({super.key});
-
-  @override
-  State<FutureBuilderExample> createState() => _FutureBuilderExampleState();
-}
-
-class _FutureBuilderExampleState extends State<FutureBuilderExample> {
-  final Future<String> _calculation = helloWorld();
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.titleLarge!,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future: _calculation, // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            children = <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('${snapshot.data}'),
-              ),
-            ];
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              ),
-            ];
-          } else {
-            children = const <Widget>[
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Awaiting result...'),
-              ),
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        },
-      ),
+      home: const ContentList(),
     );
   }
 }
